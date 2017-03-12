e04c3ac75b88339fdc531cf73964acef264e40cd
matt <mcc272@cornell.edu>
2017-03-11 22:13:06 -0800

    added about section of charity details page

--- js/details.js
+++ js/details.js
@@ -37,7 +37,46 @@ function toggleSideSelection(elemId){
 }
 
 function showAboutView(){
+  var sessObj = getSessionObject();
+  var charityId = sessObj["detailsCharity"];
+  var charityDetails = getCharityDetails();
+  var charityDetail = charityDetails[charityId];
 
+  var contentView = document.getElementById("mainContentView");
+  contentView.innerHTML = "";
+
+  var glanceHeader = document.createElement("h2");
+  glanceHeader.innerHTML = "At a Glance";
+  contentView.appendChild(glanceHeader);
+
+  var missionHeader = document.createElement("h2");
+  missionHeader.innerHTML = "Mission";
+  contentView.appendChild(missionHeader);
+
+  var missionContent = document.createElement("p");
+  missionContent.innerHTML = charityDetail["mission"];
+  contentView.appendChild(missionContent);
+
+  var newsHeader = document.createElement("h2");
+  newsHeader.innerHTML = "Recent Updates";
+  contentView.appendChild(newsHeader);
+
+  for (var i=0; i < charityDetail["news"].length; i++){
+    var date = charityDetail["news"][i].date;
+    var headline = charityDetail["news"][i].headline;
+    console.log(headline);
+    var row = document.createElement("div");
+    row.className = "row news-row";
+    contentView.appendChild(row);
+    var leftSide = document.createElement("div");
+    leftSide.className = "col-md-2";
+    leftSide.innerHTML = date;
+    row.appendChild(leftSide);
+    var rightSide = document.createElement("div");
+    rightSide.className = "col-md-10";
+    rightSide.innerHTML = "<a>" + headline + "<\a>";
+    row.appendChild(rightSide);
+  }
 }
 
 function showImpactView(){
