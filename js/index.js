2029c3fae7c471851d7b45e162ce7aed6dd47dbc
lmkostas <lmkostas@stanford.edu>
2017-03-12 23:57:15 -0700

    better opacity

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function preview(e){
-	e.childNodes[1].childNodes[5].style.opacity = '0.5'
+	e.childNodes[1].childNodes[5].style.opacity = '0.2'
 	e.childNodes[1].childNodes[3].style.display = 'none'
 	e.childNodes[1].childNodes[1].style.opacity = '1'
 	e.childNodes[1].childNodes[1].style.visibility = 'visible'
